thomasjo/atom-latex

commit 9813c52658cb13e6c7d8ab7a42163b9503925c5b

:lipstick: + :boom: = :dancers:

lib/master-tex-finder.coffee
@@ -15,7 +15,8 @@ class MasterTexFinder
   # @param filePath: a file name in the directory to be searched
   constructor: (filePath) ->
     @filePath = filePath
-    @projectPath = path.dirname(@filePath)
+    @fileName = path.basename(filePath)
+    @projectPath = path.dirname(filePath)
 
   # Returns the list of tex files in the project directory
   getTexFilesList: ->
@@ -37,21 +38,22 @@ class MasterTexFinder
 
   # Returns the list of tex files in the directory where @filePath lives that
   # contain a documentclass declaration.
-  getHeuristicSearchMasterFile: ->
+  searchForMasterFile: ->
     files = @getTexFilesList()
-    return @filePath if files.length == 0
+    return unless files?
+    return @fileName if files.length == 0
     return files[0] if files.length == 1
 
-    result = []
-    for masterCandidate in files
-      if @isMasterFile(path.join(@projectPath, masterCandidate))
-        result.push(path.join(@projectPath, masterCandidate))
-
-    if result.length == 1
-      return result[0]
+    # result = []
+    # for masterCandidate in files
+    #   candidatePath = path.join(@projectPath, masterCandidate)
+    #   if @isMasterFile(candidatePath)
+    #     result.push(candidatePath)
+    result = files.filter (name) => @isMasterFile(path.join(@projectPath, name))
+    return result[0] if result.length == 1
 
     console.warn "Cannot find latex master file" unless atom.inSpecMode()
-    @filePath
+    @fileName
 
   # Returns the a latex master file.
   #
@@ -63,6 +65,6 @@ class MasterTexFinder
   getMasterTexPath: ->
     masterPath = @getMagicCommentMasterFile()
     return masterPath if masterPath?
-    return @filePath if @isMasterFile(@filePath)
+    return @fileName if @isMasterFile(@filePath)
 
-    @getHeuristicSearchMasterFile()
+    @searchForMasterFile()

spec/master-tex-finder-spec.coffee
@@ -9,39 +9,45 @@ describe "MasterTexFinder", ->
 
   describe "getMasterTexPath", ->
     it "returns the master tex file for the current project", ->
-      expectedPath = path.join(fixturesPath, "master.tex")
       inc2Path = path.join(fixturesPath, "inc2.tex")
       finder = new MasterTexFinder(inc2Path)
-      expect(finder.getMasterTexPath()).toEqual(expectedPath)
+
+      expect(finder.getMasterTexPath()).toEqual("master.tex")
 
     it "immediately return the given file, if itself is a root-file", ->
       masterFile = path.join(fixturesPath, "master.tex")
       finder = new MasterTexFinder(masterFile)
+
       spyOn(finder, "getTexFilesList").andCallThrough()
-      expect(finder.getMasterTexPath()).toEqual(masterFile)
+
+      expect(finder.getMasterTexPath()).toEqual("master.tex")
       expect(finder.getTexFilesList).not.toHaveBeenCalled()
 
     it "returns the original file if more than one file is a master file", ->
       multiMasterFixturePath = path.join(atom.project.getPath(), "master-tex-finder", "multiple-masters")
-      inc1Path = path.join(multiMasterFixturePath, "inc1.tex")
-      finder = new MasterTexFinder(inc1Path)
       master1Path = path.join(multiMasterFixturePath, "master1.tex")
       master2Path = path.join(multiMasterFixturePath, "master2.tex")
-      expect(finder.getMasterTexPath()).toEqual(inc1Path)
+      inc1Path = path.join(multiMasterFixturePath, "inc1.tex")
+      finder = new MasterTexFinder(inc1Path)
+
+      expect(finder.getMasterTexPath()).toEqual("inc1.tex")
 
     it "immediately returns the file specified by the magic comment when present", ->
       inc1Path = path.join(fixturesPath, "inc1.tex")
       finder = new MasterTexFinder(inc1Path)
+
       spyOn(finder, "getTexFilesList").andCallThrough()
+
       expect(finder.getMasterTexPath()).toEqual("master.tex")
       expect(finder.getTexFilesList).not.toHaveBeenCalled()
 
   describe "isMasterFile", ->
     it "returns true if the given file is the master file", ->
+      masterFilePath = path.join(fixturesPath, "master.tex")
       inc2Path = path.join(fixturesPath, "inc2.tex")
       finder = new MasterTexFinder(inc2Path)
-      masterFilePath = path.join(fixturesPath, "master.tex")
-      expect(finder.isMasterFile(masterFilePath)).toBe true
+
+      expect(finder.isMasterFile(masterFilePath)).toEqual(true)
 
   describe "getTexFilesList", ->
     it "returns the list of tex files in the project directory", ->
@@ -49,4 +55,5 @@ describe "MasterTexFinder", ->
       inc2Path = path.join(fixturesPath, "inc2.tex")
       finder = new MasterTexFinder(inc2Path)
       sortedFileList = finder.getTexFilesList().sort()
+
       expect(sortedFileList).toEqual(expectedFileList)

lib/builder.coffee
@@ -3,7 +3,9 @@ _ = require "underscore-plus"
 module.exports =
 class Builder
   constructor: ->
-    @envPathKey = unless process.env.platform == "win32" then "PATH" else "Path"
+    @envPathKey = switch process.env.platform
+      when "win32" then "Path"
+      else "PATH"
 
   run: -> null
   constructArgs: -> null

spec/latex-spec.coffee
@@ -2,9 +2,9 @@ fs = require "fs-plus"
 path = require "path"
 temp = require "temp"
 wrench = require "wrench"
-latex = require "../lib/latex"
-
 {View, WorkspaceView} = require "atom"
+latex = require "../lib/latex"
+Builder = require "../lib/builder"
 
 class StatusBarMock extends View
   @content: ->
@@ -14,7 +14,7 @@ class StatusBarMock extends View
   attach: -> atom.workspaceView.appendToTop(this)
   prependRight: (view) -> @rightPanel.append(view)
 
-fakeBuilder =
+class BuilderMock extends Builder
   run: (args, callback) -> callback(0)
   constructArgs: (filePath) -> return []
   constructPath: -> return ""
@@ -32,11 +32,11 @@ describe "Latex", ->
     atom.workspace = atom.workspaceView.model
 
     # Ensure package has sensible config values
-    atom.config.set("latex.texPath", "$PATH:/usr/texbin")
+    atom.config.set("latex.texPath", "")
     atom.config.set("latex.outputDirectory", "output")
     atom.config.set("latex.enableShellEscape", false)
 
-    spyOn(latex, "getBuilder").andReturn(fakeBuilder)
+    spyOn(latex, "getBuilder").andReturn(new BuilderMock)
 
   describe "build", ->
     it "does nothing for new, unsaved files", ->

commit 22c0656a69b02cd9cea181b26883acff99a5ca6e

:bug: Fix bug introduced in 9813c52
Fixes #25

lib/master-tex-finder.coffee
@@ -1,4 +1,4 @@
-fs = require "fs"
+fs = require "fs-plus"
 path = require "path"
 MagicParser = require "./parsers/magic-parser"
 
@@ -20,7 +20,7 @@ class MasterTexFinder
 
   # Returns the list of tex files in the project directory
   getTexFilesList: ->
-    fs.readdirSync(@projectPath).filter (name) -> name.endsWith(".tex")
+    fs.listSync(@projectPath, [".tex"])
 
   # Returns true iff path is a master file (contains the documentclass declaration)
   isMasterFile: (filePath) ->
@@ -34,26 +34,24 @@ class MasterTexFinder
   # Returns null if no magic comment can be found in @filePath.
   getMagicCommentMasterFile: ->
     magic = new MagicParser(@filePath).parse()
-    magic?.root
+    masterPath = magic?.root
+    return unless masterPath?.length
+
+    masterPath = path.resolve(@projectPath, masterPath)
 
   # Returns the list of tex files in the directory where @filePath lives that
   # contain a documentclass declaration.
   searchForMasterFile: ->
     files = @getTexFilesList()
     return unless files?
-    return @fileName if files.length == 0
+    return @filePath if files.length == 0
     return files[0] if files.length == 1
 
-    # result = []
-    # for masterCandidate in files
-    #   candidatePath = path.join(@projectPath, masterCandidate)
-    #   if @isMasterFile(candidatePath)
-    #     result.push(candidatePath)
-    result = files.filter (name) => @isMasterFile(path.join(@projectPath, name))
+    result = files.filter (path) => @isMasterFile(path)
     return result[0] if result.length == 1
 
     console.warn "Cannot find latex master file" unless atom.inSpecMode()
-    @fileName
+    @filePath
 
   # Returns the a latex master file.
   #
@@ -65,6 +63,6 @@ class MasterTexFinder
   getMasterTexPath: ->
     masterPath = @getMagicCommentMasterFile()
     return masterPath if masterPath?
-    return @fileName if @isMasterFile(@filePath)
+    return @filePath if @isMasterFile(@filePath)
 
     @searchForMasterFile()

spec/master-tex-finder-spec.coffee
@@ -12,7 +12,7 @@ describe "MasterTexFinder", ->
       inc2Path = path.join(fixturesPath, "inc2.tex")
       finder = new MasterTexFinder(inc2Path)
 
-      expect(finder.getMasterTexPath()).toEqual("master.tex")
+      expect(finder.getMasterTexPath()).toEqual(path.join(fixturesPath, "master.tex"))
 
     it "immediately return the given file, if itself is a root-file", ->
       masterFile = path.join(fixturesPath, "master.tex")
@@ -20,7 +20,7 @@ describe "MasterTexFinder", ->
 
       spyOn(finder, "getTexFilesList").andCallThrough()
 
-      expect(finder.getMasterTexPath()).toEqual("master.tex")
+      expect(finder.getMasterTexPath()).toEqual(masterFile)
       expect(finder.getTexFilesList).not.toHaveBeenCalled()
 
     it "returns the original file if more than one file is a master file", ->
@@ -30,7 +30,7 @@ describe "MasterTexFinder", ->
       inc1Path = path.join(multiMasterFixturePath, "inc1.tex")
       finder = new MasterTexFinder(inc1Path)
 
-      expect(finder.getMasterTexPath()).toEqual("inc1.tex")
+      expect(finder.getMasterTexPath()).toEqual(inc1Path)
 
     it "immediately returns the file specified by the magic comment when present", ->
       inc1Path = path.join(fixturesPath, "inc1.tex")
@@ -38,7 +38,7 @@ describe "MasterTexFinder", ->
 
       spyOn(finder, "getTexFilesList").andCallThrough()
 
-      expect(finder.getMasterTexPath()).toEqual("master.tex")
+      expect(finder.getMasterTexPath()).toEqual(path.join(fixturesPath, "master.tex"))
       expect(finder.getTexFilesList).not.toHaveBeenCalled()
 
   describe "isMasterFile", ->
@@ -52,6 +52,7 @@ describe "MasterTexFinder", ->
   describe "getTexFilesList", ->
     it "returns the list of tex files in the project directory", ->
       expectedFileList = ["inc1.tex", "inc2.tex", "inc3.tex", "master.tex"]
+      expectedFileList = expectedFileList.map (name) => path.join(fixturesPath, name)
       inc2Path = path.join(fixturesPath, "inc2.tex")
       finder = new MasterTexFinder(inc2Path)
       sortedFileList = finder.getTexFilesList().sort()