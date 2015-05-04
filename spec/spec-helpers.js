cf07d9e942e78dad0a782741326db94fc09ab0c8
Thomas Johansen <thomasjo@gmail.com>
2015-05-04 23:44:22 +0200

    Export spec helpers in default object

--- spec/spec-helpers.js
+++ spec/spec-helpers.js
@@ -6,32 +6,34 @@ import fs from "fs-plus";
 import temp from "temp";
 import wrench from "wrench";
 
-export function cloneFixtures() {
-  const tempPath = fs.realpathSync(temp.mkdirSync("latex"));
-  let fixturesPath = atom.project.getPaths()[0];
-  wrench.copyDirSyncRecursive(fixturesPath, tempPath, {forceDelete: true});
-  atom.project.setPaths([tempPath]);
-  fixturesPath = tempPath;
+export default {
+  cloneFixtures() {
+    const tempPath = fs.realpathSync(temp.mkdirSync("latex"));
+    let fixturesPath = atom.project.getPaths()[0];
+    wrench.copyDirSyncRecursive(fixturesPath, tempPath, {forceDelete: true});
+    atom.project.setPaths([tempPath]);
+    fixturesPath = tempPath;
 
-  return fixturesPath;
-}
+    return fixturesPath;
+  },
 
-export function overridePlatform(name) {
-  Object.defineProperty(process, "platform", {__proto__: null, value: name});
-}
+  overridePlatform(name) {
+    Object.defineProperty(process, "platform", {__proto__: null, value: name});
+  },
 
-export function spyOnConfig(key, value) {
-  const get = atom.config.get;
-  global.spyOn(atom.config, "get").andCallFake(_key => {
-    if (_key === key) { return value; }
-    return get.call(atom.config, key);
-  });
-}
+  spyOnConfig(key, value) {
+    const get = atom.config.get;
+    global.spyOn(atom.config, "get").andCallFake(_key => {
+      if (_key === key) { return value; }
+      return get.call(atom.config, key);
+    });
+  },
 
-export function setTimeoutInterval(interval) {
-  const env = global.jasmine.getEnv();
-  const originalInterval = env.defaultTimeoutInterval;
-  env.defaultTimeoutInterval = interval;
+  setTimeoutInterval(interval) {
+    const env = global.jasmine.getEnv();
+    const originalInterval = env.defaultTimeoutInterval;
+    env.defaultTimeoutInterval = interval;
 
-  return originalInterval;
-}
+    return originalInterval;
+  },
+};
