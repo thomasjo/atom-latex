25e4cd3a18d152fae9ee36cabcb680fef695cb87
Thomas Johansen <thomasjo@gmail.com>
2015-11-09 15:21:53 +0100

    Handle ambigious builder registrations

--- spec/builder-spec.js
+++ spec/builder-spec.js
@@ -113,14 +113,26 @@ describe('Builder', () => {
   })
 
   describe('::getBuilder', () => {
-    it('returns an empty list when no builders are associated with the given file', () => {
-      const filePath = path.join('foo', 'quux.txt')
-      expect(Builder.getBuilder(filePath)).toEqual([])
+    beforeEach(() => {
+      helpers.spyOnConfig('latex.builder', 'latexmk')
     })
 
-    it('returns a non-empty list when there are builders associated with the given file', () => {
+    it('returns null when no builders are associated with the given file', () => {
+      const filePath = path.join('foo', 'quux.txt')
+      expect(Builder.getBuilder(filePath)).toBeNull()
+    })
+
+    it('returns the configured builder when given a regular .tex file', () => {
       const filePath = path.join('foo', 'bar.tex')
-      expect(Builder.getBuilder(filePath)).not.toEqual([])
+      expect(Builder.getBuilder(filePath).name).toEqual('LatexmkBuilder')
+
+      helpers.spyOnConfig('latex.builder', 'texify')
+      expect(Builder.getBuilder(filePath).name).toEqual('TexifyBuilder')
+    })
+
+    it('throws an error when unable to resolve ambigous builder registration', () => {
+      helpers.spyOnConfig('latex.builder', 'foo')
+      expect(() => { Builder.getBuilder(filePath) }).toThrow()
     })
   })
 })
