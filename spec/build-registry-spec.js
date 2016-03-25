9aa6a3e887504a04d312e2c9f35917ced3338701
Thomas Johansen <thomasjo@gmail.com>
2016-03-25 13:53:38 +0100

    Make BuilderRegistry non-static

--- spec/build-registry-spec.js
+++ spec/build-registry-spec.js
@@ -5,34 +5,35 @@ import path from 'path'
 import BuilderRegistry from '../lib/builder-registry'
 
 describe('BuilderRegistry', () => {
-  let fixturesPath, filePath
+  let registry, fixturesPath, filePath
 
   beforeEach(() => {
+    registry = new BuilderRegistry()
     fixturesPath = helpers.cloneFixtures()
     filePath = path.join(fixturesPath, 'file.tex')
   })
 
-  describe('::getBuilder', () => {
+  describe('getBuilder', () => {
     beforeEach(() => {
       helpers.spyOnConfig('latex.builder', 'latexmk')
     })
 
     it('returns null when no builders are associated with the given file', () => {
       const filePath = path.join('foo', 'quux.txt')
-      expect(BuilderRegistry.getBuilder(filePath)).toBeNull()
+      expect(registry.getBuilder(filePath)).toBeNull()
     })
 
     it('returns the configured builder when given a regular .tex file', () => {
       const filePath = path.join('foo', 'bar.tex')
-      expect(BuilderRegistry.getBuilder(filePath).name).toEqual('LatexmkBuilder')
+      expect(registry.getBuilder(filePath).name).toEqual('LatexmkBuilder')
 
       helpers.spyOnConfig('latex.builder', 'texify')
-      expect(BuilderRegistry.getBuilder(filePath).name).toEqual('TexifyBuilder')
+      expect(registry.getBuilder(filePath).name).toEqual('TexifyBuilder')
     })
 
     it('throws an error when unable to resolve ambigious builder registration', () => {
       helpers.spyOnConfig('latex.builder', 'foo')
-      expect(() => { BuilderRegistry.getBuilder(filePath) }).toThrow()
+      expect(() => { registry.getBuilder(filePath) }).toThrow()
     })
   })
 })
