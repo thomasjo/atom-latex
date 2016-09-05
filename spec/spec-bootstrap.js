9dc3fbaebf0d5d5fc2f2003488370f0b127f5359
Thomas Johansen <thomasjo@gmail.com>
2016-09-05 12:26:54 +0200

    :art: Prefer spaces in object destructuring assignments

--- spec/spec-bootstrap.js
+++ spec/spec-bootstrap.js
@@ -1,7 +1,7 @@
 'use babel'
 
 import Latex from '../lib/latex'
-import {NullLogger} from './stubs'
+import { NullLogger } from './stubs'
 
 global.latex = new Latex()
 latex.setLogger(new NullLogger())
