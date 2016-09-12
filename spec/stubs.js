3a79cf333e5e904873c71031184abe38aaf13c87
Tarn Burton <twburton@gmail.com>
2016-09-12 17:10:45 -0400

    Remove unused code and add texlive default

--- spec/stubs.js
+++ spec/stubs.js
@@ -6,7 +6,6 @@ import Opener from '../lib/opener'
 
 export class NullBuilder extends Builder {
   static canProcess (filePath) { return true }
-  run () { return new Promise(resolve => resolve(0)) }
 }
 
 export class NullLogger extends Logger {
