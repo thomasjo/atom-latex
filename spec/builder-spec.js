2db67320089e232c1f93f7500e560d65208cb588
Tarn Burton <twburton@gmail.com>
2016-09-24 14:04:43 -0400

    Normalize path in Builder and add win32 spec log

--- spec/builder-spec.js
+++ spec/builder-spec.js
@@ -5,12 +5,13 @@ import path from 'path'
 import Builder from '../lib/builder'
 
 describe('Builder', () => {
-  let builder, fixturesPath, filePath, logFilePath, fdbFilePath, magicOverrideFilePath
+  let builder, fixturesPath, filePath, win32_filePath, logFilePath, fdbFilePath, magicOverrideFilePath
 
   beforeEach(() => {
     builder = new Builder()
     fixturesPath = helpers.cloneFixtures()
     filePath = path.join(fixturesPath, 'file.tex')
+    win32_filePath = path.join(fixturesPath, 'file_win32.tex')
     logFilePath = path.join(fixturesPath, 'file.log')
     fdbFilePath = path.join(fixturesPath, 'file.fdb_latexmk')
     magicOverrideFilePath = path.join(fixturesPath, 'magic-comments', 'override-settings.tex')
@@ -136,9 +137,9 @@ describe('Builder', () => {
   describe('parseLogAndFdbFiles', () => {
     it('verifys that fdb file output overrides log file output', () => {
       spyOn(builder, 'getLogParser').andReturn({ parse: () => ({ outputFilePath: 'bar.pdf' }) })
-      const result = builder.parseLogAndFdbFiles(process.platform === 'win32' ? filePath.replace('file', 'file_win32') : filePath)
+      const result = builder.parseLogAndFdbFiles(process.platform === 'win32' ? win32_filePath : filePath)
 
-      expect(result.outputFilePath).toEqual(process.platform === 'win32' ? 'C:\\foo\\output/file.pdf' : '/foo/output/file.pdf')
+      expect(result.outputFilePath).toEqual(process.platform === 'win32' ? 'C:\\foo\\output\\file.pdf' : '/foo/output/file.pdf')
     })
   })
 
