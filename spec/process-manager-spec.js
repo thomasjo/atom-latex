44a1d3701780640727a0393b396e37766f781628
Tarn Burton <twburton@gmail.com>
2016-10-08 06:55:36 -0400

    Add Process Manager (#244)
    Add `ProcessManager` which tracks running process and allows users to kill processes with `latex:kill` or the status indicator during a running build. Also, auto-kills processes at the start of a build and adds a tooltip to the status indicator.
    
    Resolves #236, resolves #229.

---
+++ spec/process-manager-spec.js
@@ -0,0 +1,47 @@
+/** @babel */
+
+import fs from 'fs-plus'
+import temp from 'temp'
+import path from 'path'
+import ProcessManager from '../lib/process-manager'
+
+describe('ProcessManager', () => {
+  let processManager
+
+  function constructCommand (fileName) {
+    const tempPath = fs.realpathSync(temp.mkdirSync('latex'))
+    const filePath = path.join(tempPath, fileName)
+    return `latexmk -cd -f -pdf "${filePath}"`
+  }
+
+  beforeEach(() => {
+    processManager = new ProcessManager()
+  })
+
+  describe('ProcessManager', () => {
+    it('kills latexmk when given non-existant file', () => {
+      let killed = false
+
+      processManager.executeChildProcess(constructCommand('foo.tex')).then(result => { killed = true })
+      processManager.killChildProcesses()
+
+      waitsFor(() => killed, 5000)
+    })
+
+    it('kills old latexmk instances, but not ones created after the kill command', () => {
+      let oldKilled = false
+      let newKilled = false
+
+      processManager.executeChildProcess(constructCommand('old.tex')).then(result => { oldKilled = true })
+      processManager.killChildProcesses()
+      processManager.executeChildProcess(constructCommand('new.tex')).then(result => { newKilled = true })
+
+      waitsFor(() => oldKilled, 5000)
+
+      runs(() => {
+        expect(newKilled).toBe(false)
+        processManager.killChildProcesses()
+      })
+    })
+  })
+})
