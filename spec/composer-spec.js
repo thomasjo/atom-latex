0b2cb90608723d72b646d9cba4698ea64e4b3373
Tarn Burton <twburton@gmail.com>
2016-10-10 10:29:21 -0400

    Add sync tests

--- spec/composer-spec.js
+++ spec/composer-spec.js
@@ -271,4 +271,64 @@ describe('Composer', () => {
       expect(composer.getBuilder(filePath)).toBeNull()
     })
   })
+
+  describe('sync', () => {
+    it('silently does nothing when the current editor is transient', () => {
+      spyOn(werkzeug, 'getEditorDetails').andReturn({ filePath: null })
+      spyOn(composer, 'resolveOutputFilePath').andCallThrough()
+      spyOn(latex, 'getOpener').andCallThrough()
+
+      composer.sync()
+
+      expect(composer.resolveOutputFilePath).not.toHaveBeenCalled()
+      expect(latex.getOpener).not.toHaveBeenCalled()
+    })
+
+    it('logs a warning and returns when an output file cannot be resolved', () => {
+      spyOn(werkzeug, 'getEditorDetails').andReturn({ filePath: 'file.tex', lineNumber: 1 })
+      spyOn(composer, 'resolveOutputFilePath').andReturn()
+      spyOn(latex, 'getOpener').andCallThrough()
+      spyOn(latex.log, 'warning').andCallThrough()
+
+      composer.sync()
+
+      expect(latex.log.warning).toHaveBeenCalled()
+      expect(latex.getOpener).not.toHaveBeenCalled()
+    })
+
+    it('launches the opener using editor metadata and resolved output file', () => {
+      const filePath = 'file.tex'
+      const lineNumber = 1
+      const outputFilePath = 'file.pdf'
+      spyOn(werkzeug, 'getEditorDetails').andReturn({ filePath, lineNumber })
+      spyOn(composer, 'resolveOutputFilePath').andReturn(outputFilePath)
+
+      const opener = jasmine.createSpyObj('MockOpener', ['open'])
+      spyOn(latex, 'getOpener').andReturn(opener)
+
+      composer.sync()
+
+      expect(opener.open).toHaveBeenCalledWith(outputFilePath, filePath, lineNumber)
+    })
+
+    it('launches the opener using editor metadata and resolved output file with jobnames', () => {
+      const filePath = 'file.tex'
+      const lineNumber = 1
+      const rootFilePath = filePath
+      const builder = jasmine.createSpyObj('MockBuilder', ['run', 'constructArgs', 'parseLogAndFdbFiles', 'getJobNamesFromMagic'])
+      const jobnames = ['foo', 'bar']
+
+      spyOn(werkzeug, 'getEditorDetails').andReturn({ filePath, lineNumber })
+      spyOn(composer, 'resolveOutputFilePath').andCallFake((builder, rootFilePath, jobname) => jobname + '.pdf')
+      spyOn(composer, 'initializeBuild').andReturn({ rootFilePath, builder, jobnames })
+
+      const opener = jasmine.createSpyObj('MockOpener', ['open'])
+      spyOn(latex, 'getOpener').andReturn(opener)
+
+      composer.sync()
+
+      expect(opener.open).toHaveBeenCalledWith('foo.pdf', filePath, lineNumber)
+      expect(opener.open).toHaveBeenCalledWith('bar.pdf', filePath, lineNumber)
+    })
+  })
 })
