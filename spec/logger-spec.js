dbf3c36932679b2ae8ab271ad8b526616b5a8dbe
Tarn Burton <twburton@gmail.com>
2017-05-31 15:48:55 -0400

    Add some more tests

--- spec/logger-spec.js
+++ spec/logger-spec.js
@@ -11,7 +11,7 @@ describe('Logger', () => {
     logger = new Logger()
   })
 
-  describe('isMessageTypeVisible', () => {
+  describe('getMessages', () => {
     beforeEach(() => {
       logger.info()
       logger.warning()
@@ -24,7 +24,7 @@ describe('Logger', () => {
 
       expect(counts.error).toBeDefined()
       expect(counts.warning).toBeDefined()
-      expect(counts.info).toBe(1)
+      expect(counts.info).toBeDefined()
     })
 
     it('verifies info messages filtered when logging level set to warning', () => {
@@ -43,6 +43,41 @@ describe('Logger', () => {
       expect(counts.error).toBeDefined()
       expect(counts.warning).toBeUndefined()
       expect(counts.info).toBeUndefined()
+    })
+
+    it('verifies no messages filtered when useFilters is false', () => {
+      atom.config.set('latex.loggingLevel', 'error')
+      counts = _.countBy(logger.getMessages(false), 'type')
+
+      expect(counts.error).toBeDefined()
+      expect(counts.warning).toBeDefined()
+      expect(counts.info).toBeDefined()
+    })
+  })
+
+  describe('isMessageTypeVisible', () => {
+    it('verifies isMessageTypeVisible is true for all levels when logging level set to info', () => {
+      atom.config.set('latex.loggingLevel', 'info')
+
+      expect(logger.isMessageTypeVisible('info')).toBe(true)
+      expect(logger.isMessageTypeVisible('warning')).toBe(true)
+      expect(logger.isMessageTypeVisible('error')).toBe(true)
+    })
+
+    it('verifies isMessageTypeVisible is false for info when logging level set to warning', () => {
+      atom.config.set('latex.loggingLevel', 'warning')
+
+      expect(logger.isMessageTypeVisible('info')).toBe(false)
+      expect(logger.isMessageTypeVisible('warning')).toBe(true)
+      expect(logger.isMessageTypeVisible('error')).toBe(true)
+    })
+
+    it('verifies isMessageTypeVisible is false for info when logging level set to warning', () => {
+      atom.config.set('latex.loggingLevel', 'error')
+
+      expect(logger.isMessageTypeVisible('info')).toBe(false)
+      expect(logger.isMessageTypeVisible('warning')).toBe(false)
+      expect(logger.isMessageTypeVisible('error')).toBe(true)
     })
   })
 })
