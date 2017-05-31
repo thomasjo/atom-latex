f95621f79c87425f47e96d866c45bd2a494a7281
Tarn Burton <twburton@gmail.com>
2017-05-31 18:01:20 -0400

    Add sync tests

--- spec/logger-spec.js
+++ spec/logger-spec.js
@@ -3,6 +3,7 @@
 import _ from 'lodash'
 import './spec-helpers'
 import Logger from '../lib/logger'
+import werkzeug from '../lib/werkzeug'
 
 describe('Logger', () => {
   let logger, counts
@@ -78,6 +79,41 @@ describe('Logger', () => {
       expect(logger.isMessageTypeVisible('info')).toBe(false)
       expect(logger.isMessageTypeVisible('warning')).toBe(false)
       expect(logger.isMessageTypeVisible('error')).toBe(true)
+    })
+  })
+
+  describe('sync', () => {
+    let logDock
+
+    function initializeSpies (filePath, position) {
+      logDock = jasmine.createSpyObj('LogDock', ['update'])
+      spyOn(logger, 'show').andCallFake(() => Promise.resolve(logDock))
+      spyOn(werkzeug, 'getEditorDetails').andReturn({ filePath, position })
+    }
+
+    it('silently does nothing when the current editor is transient', () => {
+      initializeSpies(null, null)
+
+      waitsForPromise(() => logger.sync())
+
+      runs(() => {
+        expect(logger.show).not.toHaveBeenCalled()
+        expect(logDock.update).not.toHaveBeenCalled()
+      })
+    })
+
+    it('shows and updates the log panel with the file path and position', () => {
+      const filePath = 'file.tex'
+      const position = [[0, 0], [0, 10]]
+
+      initializeSpies(filePath, position)
+
+      waitsForPromise(() => logger.sync())
+
+      runs(() => {
+        expect(logger.show).toHaveBeenCalled()
+        expect(logDock.update).toHaveBeenCalledWith({ filePath, position })
+      })
     })
   })
 })
