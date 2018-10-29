b54e9e2fcc59525c930ab28e9a551af86ef6b98a
Thomas Johansen <thomasjo@gmail.com>
2018-10-29 21:18:11 +0100

    Fix typing and remove lint from Parser

--- lib/parser.ts
+++ lib/parser.ts
@@ -7,7 +7,7 @@ export default abstract class Parser {
     this.filePath = filePath
   }
 
-  parse () {}
+  abstract parse (): any
 
   getLines (defaultLines: string[] | null = null) {
     if (!fs.existsSync(this.filePath)) {
@@ -15,7 +15,7 @@ export default abstract class Parser {
       throw new Error(`No such file: ${this.filePath}`)
     }
 
-    const rawFile = fs.readFileSync(this.filePath, {encoding: 'utf-8'})
+    const rawFile = fs.readFileSync(this.filePath, { encoding: 'utf-8' })
     const lines = rawFile.replace(/(\r\n)|\r/g, '\n').split('\n')
     return lines
   }
