601dee0d2371756e74e1799e74512e8cffade4b9
Thomas Johansen <thomasjo@gmail.com>
2018-10-29 20:52:13 +0100

    Convert logger.js

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,48 +1,58 @@
-/** @babel */
-
 import _ from 'lodash'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
 import { CompositeDisposable, Disposable, Emitter } from 'atom'
-import { getEditorDetails } from './werkzeug'
+// import { getEditorDetails } from './werkzeug'
 import LogDock from './views/log-dock'
 
 export default class Logger extends Disposable {
   disposables = new CompositeDisposable()
   emitter = new Emitter()
+  loggingLevel: any
+  container: HTMLDivElement
+  messages: any[]
 
   constructor () {
     super(() => this.disposables.dispose())
+
+    this.disposables.add(this.emitter)
+
     this.loggingLevel = atom.config.get('latex.loggingLevel')
     this.disposables.add(atom.config.onDidChange('latex.loggingLevel', () => {
       this.loggingLevel = atom.config.get('latex.loggingLevel')
       this.refresh()
     }))
-    this.disposables.add(this.emitter)
-    this.disposables.add(atom.workspace.addOpener(uri => {
-      if (uri === LogDock.LOG_DOCK_URI) {
-        return new LogDock()
-      }
-    }))
+
+    this.container = document.createElement('div')
+    // this.disposables.add(atom.workspace.addOpener(uri => {
+    //   if (uri === LogDock.LOG_DOCK_URI) {
+    //     // return new LogDock()
+    //     // ReactDOM.render(<LogDock />, this.container)
+    //     return this.container.firstChild
+    //   }
+    // }))
 
     this.messages = []
   }
 
-  onMessages (callback) {
+  onMessages (callback: any) {
     return this.emitter.on('messages', callback)
   }
 
-  error (text, filePath, range, logPath, logRange) {
+
+  error (text: string, filePath?: string, range?: any, logPath?: any, logRange?: any) {
     this.showMessages([{ type: 'error', text, filePath, range, logPath, logRange }])
   }
 
-  warning (text, filePath, range, logPath, logRange) {
+  warning (text: string, filePath?: string, range?: any, logPath?: any, logRange?: any) {
     this.showMessages([{ type: 'warning', text, filePath, range, logPath, logRange }])
   }
 
-  info (text, filePath, range, logPath, logRange) {
+  info (text: string, filePath?: string, range?: any, logPath?: any, logRange?: any) {
     this.showMessages([{ type: 'info', text, filePath, range, logPath, logRange }])
   }
 
-  showMessages (messages) {
+  showMessages (messages: any[]) {
     messages = messages.map(message => _.pickBy(message))
     this.messages = this.messages.concat(messages)
 
@@ -68,12 +78,12 @@ export default class Logger extends Disposable {
       : this.messages
   }
 
-  setMessages (messages) {
+  setMessages (messages: any) {
     this.messages = messages
     this.emitter.emit('messages', { messages, reset: true })
   }
 
-  messageTypeIsVisible (type) {
+  messageTypeIsVisible (type: string) {
     return type === 'error' ||
       (this.loggingLevel !== 'error' && type === 'warning') ||
       (this.loggingLevel === 'info' && type === 'info')
@@ -82,13 +92,13 @@ export default class Logger extends Disposable {
   async sync () {
     // FIXME: There should be no direct interaction with editors. The required
     //        values should be arguments.
-    const { filePath, position } = getEditorDetails()
-    if (filePath) {
-      const logDock = await this.show()
-      if (logDock) {
-        logDock.update({ filePath, position })
-      }
-    }
+    // const { filePath, position } = getEditorDetails()
+    // if (filePath) {
+    //   const logDock = await this.show()
+    //   if (logDock) {
+    //     // logDock.update({ filePath, position })
+    //   }
+    // }
   }
 
   async toggle () {
