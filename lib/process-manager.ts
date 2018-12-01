8dd09b62a6f3fd340d3b473a44380c8e0bbc0dbc
Thomas Johansen <thomasjo@gmail.com>
2018-12-02 00:00:38 +0100

    Refactor and improve process management

--- lib/process-manager.ts
+++ lib/process-manager.ts
@@ -1,6 +1,21 @@
-import childProcess from 'child_process'
+import { exec } from 'child_process'
 import kill from 'tree-kill'
 import { Disposable } from 'atom'
+
+interface ProcessResult {
+  statusCode: number
+  stdout?: string
+  stderr?: string
+}
+
+interface ProcessOptions {
+  allowKill?: boolean
+  showError?: boolean
+  encoding?: string
+  maxBuffer?: number
+  cwd?: string
+  env?: any
+}
 
 export default class ProcessManager extends Disposable {
   processes = new Set()
@@ -9,35 +24,50 @@ export default class ProcessManager extends Disposable {
     super(() => this.killChildProcesses())
   }
 
-  executeChildProcess (command: any, options: any = {}) {
+  async executeChildProcess (command: string, options?: ProcessOptions) {
+    if (!options) {
+      options = { allowKill: false, showError: false }
+    }
+
     const { allowKill, showError, ...execOptions } = options
-    return new Promise(resolve => {
-      // Windows does not like \$ appearing in command lines so only escape
-      // if we need to.
-      if (process.platform !== 'win32') command = command.replace('$', '\\$')
-      const { pid } = childProcess.exec(command, execOptions, (error: any, stdout, stderr) => {
-        if (allowKill) {
-          this.processes.delete(pid)
+
+    // Windows does not like \$ appearing in command lines so only escape if we need to.
+    if (process.platform !== 'win32') {
+      command = command.replace('$', '\\$')
+    }
+
+    const promise: Promise<ProcessResult> = new Promise((resolve) => {
+      const { pid } = exec(command, execOptions, (error, stdout, stderr) => {
+        let statusCode = 0
+
+        if (error) {
+          statusCode = error.code!
+
+          if (showError && latex && latex.log) {
+            latex.log.error(`An error occurred while trying to run "${command}" (${error.code}).`)
+          }
         }
-        if (error && showError && latex && latex.log) {
-          latex.log.error(`An error occurred while trying to run "${command}" (${error.code}).`)
-        }
+
         resolve({
-          statusCode: error ? error.code : 0,
-          stdout,
-          stderr
+          statusCode,
+          stdout: stdout.toString(),
+          stderr: stderr.toString()
         })
       })
+
       if (allowKill) {
         this.processes.add(pid)
       }
     })
+
+    return promise
   }
 
   killChildProcesses () {
     for (const pid of this.processes.values()) {
       kill(pid)
     }
+
     this.processes.clear()
   }
 }
