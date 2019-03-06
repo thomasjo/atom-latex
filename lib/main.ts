098527a0645ce0ce317dbd0538e54803af64e9e3
Thomas Johansen <thomasjo@gmail.com>
2019-03-06 07:21:14 +0100

    Remove lint and reformat

--- lib/main.ts
+++ lib/main.ts
@@ -1,80 +1,81 @@
-import { CompositeDisposable, Disposable } from 'atom'
-import MarkerManager from './marker-manager'
+import { CompositeDisposable, Disposable } from "atom";
 
-let disposables = new CompositeDisposable()
+import MarkerManager from "./marker-manager";
 
-export function activate (serialized: any) {
-  bootstrap()
+let disposables = new CompositeDisposable();
+
+export function activate(serialized: any) {
+  bootstrap();
 
   if (serialized && serialized.messages) {
-    latex.log.setMessages(serialized.messages)
+    latex.log.setMessages(serialized.messages);
   }
 
-  disposables.add(atom.commands.add('atom-workspace', {
-    'latex:build': () => latex.composer.build(false),
-    'latex:check-runtime': () => checkRuntime(),
-    'latex:clean': () => latex.composer.clean(),
-    'latex:clear-log': () => latex.log.clear(),
-    'latex:hide-log': () => latex.log.hide(),
-    'latex:kill': () => latex.composer.kill(),
-    'latex:rebuild': () => latex.composer.build(true),
-    'latex:show-log': () => latex.log.show(),
-    'latex:sync-log': () => latex.log.sync(),
-    'latex:sync': () => latex.composer.sync(),
-    'latex:toggle-log': () => latex.log.toggle()
-  }))
+  disposables.add(atom.commands.add("atom-workspace", {
+    "latex:build": () => latex.composer.build(false),
+    "latex:check-runtime": () => checkRuntime(),
+    "latex:clean": () => latex.composer.clean(),
+    "latex:clear-log": () => latex.log.clear(),
+    "latex:hide-log": () => latex.log.hide(),
+    "latex:kill": () => latex.composer.kill(),
+    "latex:rebuild": () => latex.composer.build(true),
+    "latex:show-log": () => latex.log.show(),
+    "latex:sync": () => latex.composer.sync(),
+    "latex:sync-log": () => latex.log.sync(),
+    "latex:toggle-log": () => latex.log.toggle(),
+  }));
 
-  disposables.add(atom.workspace.observeTextEditors(editor => {
+  disposables.add(atom.workspace.observeTextEditors((editor) => {
     disposables.add(editor.onDidSave(async () => {
       // Let's play it safe; only trigger builds for the active editor.
-      const activeEditor = atom.workspace.getActiveTextEditor()
-      if (editor === activeEditor && atom.config.get('latex.buildOnSave')) {
-        await latex.composer.build(false, false)
+      const activeEditor = atom.workspace.getActiveTextEditor();
+      if (editor === activeEditor && atom.config.get("latex.buildOnSave")) {
+        await latex.composer.build(false, false);
       }
-    }))
-  }))
+    }));
+  }));
 
-  disposables.add(atom.workspace.observeTextEditors(editor => {
-    disposables.add(new MarkerManager(editor))
-  }))
+  disposables.add(atom.workspace.observeTextEditors((editor) => {
+    disposables.add(new MarkerManager(editor));
+  }));
 }
 
-export function deactivate () {
-  disposables.dispose()
-  disposables = new CompositeDisposable()
-  delete global.latex
+export function deactivate() {
+  disposables.dispose();
+  disposables = new CompositeDisposable();
+  delete global.latex;
 }
 
-export function serialize () {
-  return { messages: latex.log.getMessages(false) }
+export function serialize() {
+  return { messages: latex.log.getMessages(false) };
 }
 
-export function consumeStatusBar (statusBar: any) {
-  bootstrap()
-  latex.status.attachStatusBar(statusBar)
+export function consumeStatusBar(statusBar: any) {
+  bootstrap();
+  latex.status.attachStatusBar(statusBar);
   return new Disposable(() => {
     if (global.latex) {
-      global.latex.status.detachStatusBar()
+      global.latex.status.detachStatusBar();
     }
-  })
+  });
 }
 
-export function deserializeLog (_serialized: any) {
-  bootstrap()
-  const LogDock = require('./views/log-dock')
-  return new LogDock()
+export function deserializeLog(_serialized: any) {
+  bootstrap();
+  const LogDock = require("./views/log-dock");
+  return new LogDock();
 }
 
-function bootstrap () {
-  if (global.latex) { return }
+function bootstrap() {
+  if (global.latex) { return; }
 
-  const Latex = require('./latex').default
-  global.latex = new Latex()
-  disposables.add(global.latex)
+  const Latex = require("./latex").default;
+  global.latex = new Latex();
+  disposables.add(global.latex);
 }
 
-async function checkRuntime () {
-  latex.log.clear()
-  await latex.builderRegistry.checkRuntimeDependencies()
-  latex.opener.checkRuntimeDependencies()
+async function checkRuntime() {
+  latex.log.clear();
+  await latex.builderRegistry.checkRuntimeDependencies();
+  latex.opener.checkRuntimeDependencies();
 }
